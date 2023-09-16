1e03fff84f1f4cf13eae1ac799b3668fa625ddb1
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-09-16 20:38:50 +0500

    Create concurrent.sh

---
+++ concurrent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Purpose: To check concurrent traffic at the inverval of 10 seconds, there will be total 3 iterations
+# Author: Guman Singh | Cloudways
+# Last Edited: 16/09/2023:8:38
+
+iterations=3
+interval=10
+
+for ((i = 1; i <= iterations; i++)); do
+    echo "Iteration: $i"
+    awk '$2 ~ /:0050|:01BB/ && $4 ~ /01/ {count +=1;} END {print "Concurrent Web Connections: ",count}' /proc/net/tcp
+    sleep $interval
+done
+rm -f ./concurrent.sh
+exit
