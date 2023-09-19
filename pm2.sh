8baca814e5edfd58d8e01a92bd3fe1079056fe2c
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-09-19 12:40:21 +0500

    Update pm2.sh

--- pm2.sh
+++ pm2.sh
@@ -4,44 +4,41 @@
 # Author: Guman Singh | Cloudways
 # Last Edited: 12/09/2023:6:58
 
-# Get the current username
-current_user=$(whoami)
+# Get the current user
+CURRENT_USER=$(whoami)
 
-# Check if the current user is the master user
-if [ "$current_user" = "master" ]; then
-    # Add npm bin directory to PATH
-    echo "export PATH='$PATH:/home/master/bin/npm'" >> ~/.bash_aliases
-
-    # Add NODE_PATH configuration
-    echo "export NODE_PATH='$NODE_PATH:/home/master/bin/npm/lib/node_modules'" >> ~/.bash_aliases
-
-    # Configure npm prefix
-    npm config set prefix "/home/master/bin/npm/lib/node_modules"
-
-    # Install pm2 globally
-    npm install pm2@latest -g
-
-    # Add pm2 alias to .bash_aliases
-    echo "alias pm2='/home/master/bin/npm/lib/node_modules/bin/pm2'" >> ~/.bash_aliases
-
-    # Run pm2 startup
-    pm2 startup
-
-    # Start npm using pm2
-    pm2 start npm -- start
-
-    echo "Setup completed for the master user."
-else
-    # Display a prompt for platformops users to switch to master user
-    echo "You are currently logged in as '$current_user'."
-    read -p "Switch to the master user (master) and run the script? (y/n): " switch_user
-    master=$(awk -F: '{print $1}' /etc/passwd | grep master)
-    if [ "$switch_user" = "y" ]; then
-        su - "$master"
-    else
-        echo "Setup not completed. You can manually switch to the master user and run the script."
-    fi
+# Display a warning if the user does not start with "master_"
+if [[ $CURRENT_USER != "master_"* ]]; then
+  echo -e "\033[1;33mWarning: You are currently logged in as $CURRENT_USER. It is recommended to run this script as the 'master' user for proper configuration.\033[0m"
 fi
 
-rm -rf ./pm2.sh
+# Define the installation directory
+INSTALL_DIR="/home/master/bin/npm/lib/node_modules"
+
+# Check if pm2 is already installed
+if ! pm2 -v &>/dev/null; then
+  # Add NODE_PATH configuration
+  echo "export NODE_PATH='\$NODE_PATH:$INSTALL_DIR'" >> ~/.bash_aliases
+
+  # Configure npm prefix
+  npm config set prefix "$INSTALL_DIR"
+
+  # Install pm2 globally
+  npm install pm2@latest -g
+
+  # Add pm2 alias to .bash_aliases
+  echo "alias pm2='$INSTALL_DIR/bin/pm2'" >> ~/.bash_aliases
+
+  # Run pm2 startup
+  "$INSTALL_DIR/bin/pm2" startup
+
+  # Start npm using pm2
+  "$INSTALL_DIR/bin/pm2" start npm -- start
+
+  # Print success message in green
+  echo -e "\033[0;32mPM2 has been installed and configured successfully.\033[0m"
+else
+  echo -e "\033[0;32mPM2 is already installed.\033[0m"
+fi
+bash
 exit
