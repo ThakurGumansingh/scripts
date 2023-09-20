f270d4adfddc66748aabb7a7a3bbcbb8246bbfc2
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-09-20 20:58:28 +0500

    Update ipabuse.sh

--- ipabuse.sh
+++ ipabuse.sh
@@ -3,8 +3,20 @@
 # Author: Guman Singh | Cloudways
 # Last Edited: 20/09/2023:08:53
 
-# Bash script to extract IP addresses and save them to a text file
-curl -s https://raw.githubusercontent.com/ThakurGumansingh/scripts/main/extract.sh | bash
+# Get the current working directory
+current_dir=$(pwd)
+
+# Extract the database name from the directory path
+dbname=$(echo "$current_dir" | grep -oP '(?<=applications/)[^/]+')
+
+# Define the source and destination paths
+logs_dir="/home/master/applications/$dbname/logs"
+output_dir="/home/master/applications/$dbname/public_html"
+
+# Use find to locate log files and extract unique IP addresses
+find "$logs_dir" -type f -name "apache_*.access.log" -exec grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' {} \; | sort -u > "$output_dir/ip_addresses.txt"
+
+echo "Unique IP addresses saved in ip_addresses.txt"
 
 # Python script to check IP addresses against AbuseIPDB
 python3 <<EOF
