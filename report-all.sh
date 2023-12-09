107114fd100bb19100b5df6c19a2163beaf18457
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-12-09 20:52:10 +0500

    Create report-all.sh

---
+++ report-all.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Purpose: To generate a report that can be downloaded and contains information about the traffic, MySQL queries, and PHP pages for all applications on the server.
+# Author: Guman Singh | Cloudways
+# Last Edited: 07/11/2023:11:12
+# Usage: bash server_report.sh 5m ;The last param(5m) can be replaced by your desired timeframe that is 5m (m=minutes) or 1h (h=hours) or 10d (d=days) etc.
+
+# Check if a day value was provided as a command-line argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <day_value>"
+    exit 1
+fi
+
+dayd="$1"  # Assign the day value from the command line
+
+# Get the current working directory
+current_dir=$(pwd)
+
+# Extract the database name from the directory path
+dbname=$(echo "$current_dir" | grep -oP '(?<=applications/)[^/]+')
+
+# Change to the applications directory
+applications_dir="/home/master/applications"
+cd "$applications_dir"
+
+# Save the output to a report file in the current directory
+report_file="$applications_dir/$dbname/public_html/server_report.csv"
+for app in $(ls); do
+    echo "Application: $app" >> "$report_file"
+
+    # Traffic report
+    /usr/local/sbin/apm traffic -s "$app" -l "$dayd" >> "$report_file"
+
+    # MySQL queries report
+    echo "MySQL Queries Report" >> "$report_file"
+    sudo apm mysql -s "$app" -l "$dayd" >> "$report_file"
+
+    # PHP pages report
+    echo "PHP Pages Report" >> "$report_file"
+    sudo apm php -s "$app" -l "$dayd" >> "$report_file"
+
+    echo "" >> "$report_file"  # Add a newline between application reports
+done
+
+# Change to the website directory
+website_dir="$applications_dir/$dbname/public_html"
+cd "$website_dir"
+
+# Run the wp cli command to get the site URL
+site_url=$(wp option get siteurl --path="$website_dir")
+
+# Display the contents of the report file
+cat "$report_file"
+
+# Create a downloadable report link
+download_link="$site_url/server_report.csv"
+echo "Downloadable report link: $download_link"
+
+# Return to the original directory
+cd "$current_dir"
