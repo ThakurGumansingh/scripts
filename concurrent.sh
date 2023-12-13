bef67199a60a8149d1e295984ed0ccc126bc9d95
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-12-13 21:50:46 +0500

    Update concurrent.sh

--- concurrent.sh
+++ concurrent.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Purpose: To check concurrent traffic at the interval of 10 seconds, there will be total 3 iterations
+# Purpose: To check concurrent traffic at the interval of 3 seconds, there will be total 3 iterations
 # Author: Guman Singh | Cloudways
 # Last Edited: 13/12/2023:8:38
 
 iterations=3
 interval=3
 CT=0
+
+# Get the number of cores
+cores=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
 
 for ((i = 1; i <= iterations; i++)); do
     echo "Iteration: $i"
@@ -19,3 +22,18 @@ for ((i = 1; i <= iterations; i++)); do
 done
 
 echo "Maximum Concurrent Web Connections: $CT"
+
+# Check conditions based on the number of cores and print colorized output
+if ((cores == 2 && CT > 40)); then
+    echo -e "\e[91mServer has more concurrent traffic than the limit for 2 cores.\e[0m"  # Red color
+elif ((cores == 4 && CT > 85)); then
+    echo -e "\e[91mServer has more concurrent traffic than the limit for 4 cores.\e[0m"  # Red color
+elif ((cores == 8 && CT > 165)); then
+    echo -e "\e[91mServer has more concurrent traffic than the limit for 8 cores.\e[0m"  # Red color
+elif ((cores == 32 && CT > 325)); then
+    echo -e "\e[91mServer has more concurrent traffic than the limit for 32 cores.\e[0m"  # Red color
+elif ((cores == 64 && CT > 645)); then
+    echo -e "\e[91mServer has more concurrent traffic than the limit for 64 cores.\e[0m"  # Red color
+else
+    echo -e "\e[92mServer is within the concurrent traffic limits for its core count.\e[0m"  # Green color
+fi
