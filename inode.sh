ac9334ef2b728ac08df31aa4e5ef80d16aaecfd5
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-12-14 13:34:11 +0500

    Update inode.sh

--- inode.sh
+++ inode.sh
@@ -1,5 +1,6 @@
 # Purpose: To check what is consuming more inodes
 # Author: Guman Singh | Cloudways
 # Last Edited: 16/09/2023:8:48
+# Usage: curl -s https://raw.githubusercontent.com/ThakurGumansingh/scripts/main/inode.sh | bash
 
 { find / -xdev -printf '%h\n' | sort | uniq -c | sort -rn; } 2>/dev/null | head
