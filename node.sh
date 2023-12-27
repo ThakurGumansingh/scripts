c8d0cc1d752e148ecc24a8b83b6438992b61f0e6
Thakur Gumansingh <44533363+ThakurGumansingh@users.noreply.github.com>
2023-12-27 17:01:51 +0500

    Update node.sh

--- node.sh
+++ node.sh
@@ -11,10 +11,7 @@ print_green() {
 
 # Step 1: Download and install NVM
 curl https://gist.githubusercontent.com/cloudways-haider/f7cb6627f6674c263624589d360e12b4/raw/9d0b2c78ace5b7b2dedb411e9d676129e34b470a/nvm_install.sh | bash
-source ~/.bashrc
-
 source ~/.bashrc ~/.bash_aliases
-command -v nvm
 
 # Step 4: Delete npm prefix configuration
 npm config delete prefix
